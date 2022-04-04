6009e641b611f81254ca8dbecda7b1c8a5b236ea
Bharat-Runwal <ee3180598@ee.iitd.ac.in>
2022-04-04 21:57:20 +0530

    update

--- scripts/few_shot.sh
+++ scripts/few_shot.sh
@@ -1,17 +1,17 @@
 # Example for few shot experiments
-n_shots = $1 
+n_shots = 540 # number of shots
 
 python main_anytime_train.py \
     --data ../data \
-    --dataset cifar10 \
+    --dataset restricted_imagenet \
     --arch resnet50 \
     --seed 1 \
     --epochs 50 \
     --decreasing_lr 20,40 \
-    --batch_size 64 \
+    --batch_size 32 \
     --weight_decay 1e-4 \
-    --meta_batch_size 6250 \
-    --meta_batch_number 8 \
+    --meta_batch_size 63 \
+    --meta_batch_number 120 \
     --sparsity_level 4.5 \
     --snip_size 0.20 \
     --few_shot \
